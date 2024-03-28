b96abe80535960b6eedf8a6b48f82b17a75e2de8
Karthik <123794537+Karthiksundaram20@users.noreply.github.com>
2024-03-28 17:05:41 +0530

    Add files via upload

---
+++ lib/Features/home/forgot_Password.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../core/config/style/app_color.dart';
+
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    var whiteTextColor;
+    return  SafeArea(
+      child: Scaffold(
+        extendBody: true,
+        extendBodyBehindAppBar: true,
+        body: Container(
+          height: 800.h,
+          width: 360.w,
+          color: AppColor.appMainColor,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50.sp,
+                  width: 50.sp,
+                ),
+                Container(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      "FORGOT PASSWORD",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24.sp,
+                        fontFamily: 'inter-Bold',
+                        color: AppColor.whiteColor,
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 100.h),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 80.sp),
+                      Center(
+                        child: SizedBox(
+                          width: 340,
+                          height: 100,
+                          child: TextField(
+                            keyboardType: TextInputType.phone,
+                            decoration: InputDecoration(
+                              labelText: 'Enter Mobile Number',
+                              filled: true,
+                              labelStyle: TextStyle(fontSize: 25,fontFamily: 'inter-Bold',),
+                              fillColor: AppColor.whiteColor,
+                              border: OutlineInputBorder(
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 60.h,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: SizedBox(
+                          height: 30.h,
+                          width: 110.w,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<
+                                  Color>(
+                                  AppColor.yellowTextColor),
+                              foregroundColor:
+                              MaterialStateProperty.all<Color>(
+                                  AppColor.blackTextColor),
+                            ),
+                            child: Center(child: Text('GET OTP')),
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+
+      ),
+    );
+  }
+}
